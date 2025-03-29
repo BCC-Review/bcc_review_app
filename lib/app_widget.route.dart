d04be5c83a7bcaea305d7721363df362a5130bf5
Alex Lopes <alex.lsilva154@gmail.com>
2025-03-29 15:34:17 -0300

    Adiciona nova rota para exibir módulos e atualiza a navegação na tela inicial

--- lib/app_widget.route.dart
+++ lib/app_widget.route.dart
@@ -5,7 +5,8 @@ import 'package:routefly/routefly.dart';
 
 import 'ui/home/home_page.dart' as a2;
 import 'ui/login/login_page.dart' as a0;
-import 'ui/settings/settings_page.dart' as a3;
+import 'ui/module/[id]/show_page.dart' as a3;
+import 'ui/settings/settings_page.dart' as a4;
 import 'ui/splash/splash_page.dart' as a1;
 
 Route b0Builder(BuildContext ctx, RouteSettings settings) =>
@@ -15,4 +16,6 @@ Route b1Builder(BuildContext ctx, RouteSettings settings) =>
 Route b2Builder(BuildContext ctx, RouteSettings settings) =>
     Routefly.defaultRouteBuilder(ctx, settings, const a2.HomePage());
 Route b3Builder(BuildContext ctx, RouteSettings settings) =>
-    Routefly.defaultRouteBuilder(ctx, settings, const a3.SettingsPage());
+    Routefly.defaultRouteBuilder(ctx, settings, const a3.ModulePage());
+Route b4Builder(BuildContext ctx, RouteSettings settings) =>
+    Routefly.defaultRouteBuilder(ctx, settings, const a4.SettingsPage());
